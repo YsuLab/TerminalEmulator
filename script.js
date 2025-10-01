78423f9b32fd97d91490f1bd478eb5425fbf698d
YsuLab <67139997+YsuLab@users.noreply.github.com>
2025-10-01 17:42:40 +0300

    V0.1
    Optimized: Ping, CSS, and other adjustments
    Added: whoami, landscape promopt

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const adminPrompt = document.getElementById("adminPrompt");
 const wrapper = document.getElementById("wrapper");
 
 terminalInput.focus();
+wrapper.addEventListener("click", () => terminalInput.focus());
 
 //Loader
 const loadArray = ["|", "/", "-", "\\"];
@@ -51,7 +52,9 @@ terminalInput.addEventListener("keydown", function (event) {
       // by setting an interval and clearing it either when the pings are flag.done or after a certain time (timeout).
 
       let message =
-        " Pinging " + parts[1] + " with ~512 bytes of data (HTTP fetch) <br/>";
+        "<br/> Pinging " +
+        parts[1] +
+        " with ~2048 bytes of data (HTTP fetch) <br/>";
       terminalText.innerHTML += "<br/>" + message;
       loader(" ", flag);
 
@@ -67,19 +70,29 @@ terminalInput.addEventListener("keydown", function (event) {
             if (res === false) response = "Request timed out.";
             else {
               successful++;
-              response =
-                "Reply from " + parts[1] + " " + (new Date() - date) + " ms";
+              if (successful == 1) {
+                response =
+                  "Reply from " + parts[1] + " " + (new Date() - date) + " ms";
+                prevDate = new Date() - date;
+              } else {
+                response =
+                  "Reply from " +
+                  parts[1] +
+                  " " +
+                  (new Date() - prevDate - date) +
+                  " ms";
+              }
             }
             terminalText.innerHTML += "<br/>" + response;
-            wrapper.scrollTop = wrapper.scrollHeight;
             console.log(response);
+            wrapper.scrollTop = wrapper.scrollHeight;
 
             if (times >= 4) {
               flag.done = true;
 
               let lost = times - successful;
               terminalText.innerHTML +=
-                "<br/> Ping Statistics for " + parts[1] + ":";
+                "<br/><br/> Ping Statistics for " + parts[1] + ":";
               terminalText.innerHTML +=
                 "<br/>\tPackets: Sent = 4, Recieved = " +
                 successful +
@@ -88,11 +101,45 @@ terminalInput.addEventListener("keydown", function (event) {
                 " (" +
                 lost * 25 +
                 "% loss)";
+              wrapper.scrollTop = wrapper.scrollHeight;
+            }
+            if (times - successful >= 4) {
+              terminalText.innerHTML +=
+                "<br/><br/> Note: " +
+                parts[1] +
+                " either doesn't allow HTTPS requests (CORS) or is offline. <br/>" +
+                "Try an IP like 8.8.8.8, a domain like github.com, or even northvpn.com.";
             }
           })
-          .catch((e) => console.log(e));
-        date = new Date();
+          .catch((e) => {
+            console.log(e);
+            terminalText.innerHTML += "<br/>" + e;
+            flag.done = true;
+          });
       }
+    } else if (userInput.startsWith("whoami")) {
+      getIP().then(
+        // ip will be the returned value from getip into a lambda function
+        (userSpecs) => {
+          terminalText.innerHTML += `<br/><br/>
+          Note: This data is completly private and is only available locally, and this data depeneds on your own browser/settings and could be spoofed by it. <br/>
+          <br/> > Your Agent:: ${userSpecs.agent} 
+          <br/> > Your CGNAT IP Adress/Public IPv4 Adress:: ${
+            userSpecs.ipAdress
+          }
+          <br/> > Your Machines's Number of Threads:: ${
+            userSpecs.hardwareConcurrency
+          } Threads
+          <br/> > Your Screen's Resolution:: ${userSpecs.screenRes}
+          <br/> > Your Region/Timezone:: ${userSpecs.timezone}
+          <br/> > Your Cookie Settings:: ${
+            userSpecs.cookiesEnabled ? "Enabled" : "Disabled"
+          }
+          <br/> > Your Language/s:: ${userSpecs.languages}
+          `;
+          wrapper.scrollTop = wrapper.scrollHeight;
+        }
+      );
     } else if (userInput === "clear" || userInput === "clc") {
       terminalText.innerHTML = "";
       inputBox.value = "";
@@ -103,9 +150,13 @@ terminalInput.addEventListener("keydown", function (event) {
       // Do nothing on empty input
     } else if (userInput === "exit") {
       terminalText.innerHTML += "<br/>Logging out...";
-      setTimeout(() => {
+      (async () => {
+        await sleep(1500);
+        terminalText.innerHTML += "<br/>See You Next Time";
+
+        await sleep(2500);
         window.close();
-      }, 1500);
+      })();
     } else if (userInput === "version" || userInput === "ver") {
       terminalText.innerHTML +=
         "<br/>Terminal Emulator Version 1.0.0<br/>Developed by Yasir Aloufi <br/>";
@@ -136,25 +187,32 @@ terminalInput.addEventListener("keydown", function (event) {
     }
     // Scroll to the bottom of the terminal
     wrapper.scrollTop = wrapper.scrollHeight;
+
+    // could have used a stack to implement this oh well..
   } else if (event.key === "ArrowUp") {
     moveCursorToEnd();
     if (prompts.length === 0) return;
-    if (pointer == 0) {
+    if (pointer < 0) {
       return;
     }
+
     terminalInput.value = prompts[pointer];
-    --pointer;
+    if (pointer != 0) {
+      --pointer;
+    }
 
     console.log(prompts.length - 1);
     console.log(pointer);
+    // issue her skipping some inputs
   } else if (event.key === "ArrowDown") {
-    moveCursorToEnd();
+    console.log(prompts.length);
     if (prompts.length === 0) return;
-    else if (pointer >= prompts.length - 1) {
+    if (pointer >= prompts.length - 1) {
+      terminalInput.value = "";
       return;
     }
-    ++pointer;
     terminalInput.value = prompts[pointer];
+    ++pointer;
 
     console.log(prompts.length - 1);
     console.log(pointer);
@@ -215,4 +273,33 @@ async function loader(message, flag) {
       loadArray[loaderItem]
     );
   }, 100);
+}
+
+async function getIP() {
+  try {
+    const ua = navigator;
+    const userSpecs = {
+      agent: navigator.userAgent,
+      languages: navigator.languages,
+      screenRes: `${screen.width}x${screen.height}`,
+      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      hardwareConcurrency: navigator.hardwareConcurrency,
+      // Depracted: too clunky as it requests the user for the data
+      // clipboard: await navigator.clipboard.removeEventListener() || null,
+      cookiesEnabled: navigator.cookieEnabled,
+    };
+    const res = await fetch("https://api.ipify.org?format=json");
+
+    if (!res.ok) {
+      throw new Error(`HTTP Error: ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    userSpecs.ipAdress = data.ip;
+    console.log(typeof data.ip);
+    console.log(userSpecs);
+    return userSpecs;
+  } catch (err) {
+    console.error(`Error fetching ip: ${err}`);
+  }
 }
